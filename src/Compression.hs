2149dae096ac7d77ddbd14dfe23311b9284c1cfd
aku <aku_pohjola@hotmail.fi>
2018-03-24 21:27:56 +0200

    Added fromUnary.

--- src/Compression.hs
+++ src/Compression.hs
@@ -1,14 +1,22 @@
-module Compression (golompRize, unary) where
+module Compression (golompRize,
+                    toUnary,
+                    fromUnary) where
 
 import qualified Data.Bits as Bits
 import Data.Bits(Bits)
 import Data.BitVector(BitVector)
 import qualified Data.BitVector as BV
 
-golompRize :: Bits b => b -> BitVector
-golompRize _ = BV.bitVec 1 1
+golompRize :: Bits b => Int -> b -> BitVector
+-- compute l = x mod (2^k), h = floor(i / (2^k))
+--
+golompRize _ _ = BV.bitVec 1 1
 
 -- Unary = N-1 leading zeros and 1
-unary :: Int -> Maybe BitVector
-unary v | v > 0 = Just (BV.bitVec v 1)
-        | otherwise = Nothing
+toUnary :: Int -> Maybe BitVector
+toUnary v   | v > 0 = Just (BV.bitVec v 1)
+            | otherwise = Nothing
+
+fromUnary :: BitVector -> Maybe Int
+fromUnary bv    | BV.uint bv == 1 = Just (BV.size bv)
+                | otherwise = Nothing
--- test/CompressionSpec.hs
+++ test/CompressionSpec.hs
@@ -1,20 +1,35 @@
 module CompressionSpec (compressionTests) where
 
 import Test.HUnit
+import Data.BitVector(BitVector)
 import qualified Data.BitVector as BV
-import qualified Compression as Comp
+import qualified Compression as CP
 
 
-compressionTests = TestList [unaryTests]
+compressionTests = TestList [toUnaryTests, fromUnaryTests]
 
-unaryTests = TestList [ TestLabel "Unary 1" testUnary1,
-                        TestLabel "Unary 5" testUnary5,
-                        TestLabel "Unary 0" testUnary0,
-                        TestLabel "Unary negative" testUnaryNegative]
+toUnaryTests = TestList [ TestLabel "1 to unary" testToUnary1,
+                        TestLabel "5 to unary" testToUnary5,
+                        TestLabel "0 to unary" testToUnary0,
+                        TestLabel "Negative to unary" testToUnaryNegative]
+fromUnaryTests = TestList   [TestLabel "1 from unary" testFromUnary0,
+                            TestLabel "5 from unary" testfromUnary5,
+                            TestLabel "0 from unary" testFromUnary0,
+                            TestLabel "Negative from unary" testfromUnaryNegative]
 
-unaryTest :: Maybe String -> Int -> Test
-unaryTest res x = TestCase ( assertEqual "Bit representation" res (fmap BV.showBin . Comp.unary $ x))
-testUnary1 = unaryTest (Just "0b1") 1
-testUnary5 = unaryTest (Just "0b00001") 5
-testUnary0 = unaryTest Nothing 0
-testUnaryNegative = unaryTest Nothing (-123456)
+toUnaryTest :: Maybe String -> Int -> Test
+toUnaryTest res x = TestCase ( assertEqual "Coded value"
+                        res (fmap BV.showBin . CP.toUnary $ x))
+fromUnaryTest :: Maybe Int -> BitVector  -> Test
+fromUnaryTest res x = TestCase ( assertEqual "Uncoded value"
+                        res (CP.fromUnary x))
+
+testToUnary1 = toUnaryTest (Just "0b1") 1
+testToUnary5 = toUnaryTest (Just "0b00001") 5
+testToUnary0 = toUnaryTest Nothing 0
+testToUnaryNegative = toUnaryTest Nothing (-123456)
+
+testFromUnary1 = fromUnaryTest (Just 1) (BV.bitVec 1 1)
+testfromUnary5 = fromUnaryTest (Just 5) (BV.bitVec 5 1)
+testFromUnary0 = fromUnaryTest Nothing (BV.bitVec 1 0)
+testfromUnaryNegative = fromUnaryTest Nothing (BV.bitVec 2 (-1))
